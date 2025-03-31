66ea860df39d0d2cc4a553c4297ff834df4eec3b
Pramodya Samarakoon <pramodyasamarakoon2516@gmail.com>
2025-03-31 11:32:38 +0530

    Remove unnecessary Box import from Heading component

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 
 interface HeadingProps {
   title: string;
